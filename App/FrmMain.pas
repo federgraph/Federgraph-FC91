b75889527c9d34ce4832b930a47e609ad8f4a8e6
gs <gschub@googlemail.com>
2026-06-24 06:34:35 +0200

    New App

---
+++ App/FrmMain.pas
@@ -0,0 +1,58 @@
+﻿unit FrmMain;
+
+(*
+-
+-     F
+-    * * *
+-   *   *   G
+-  *     * *   *
+- E - - - H - - - I
+-  *     * *         *
+-   *   *   *           *
+-    * *     *             *
+-     D-------A---------------B
+-              *
+-              (C) federgraph.de
+-
+*)
+
+interface
+
+uses
+  System.SysUtils,
+  System.Types,
+  System.UITypes,
+  System.Classes,
+  FMX.Types,
+  FMX.Controls,
+  FMX.Forms,
+  FMX.Graphics,
+  FMX.Dialogs;
+
+type
+  TFormMain = class(TForm)
+    procedure FormCreate(Sender: TObject);
+  private
+  end;
+
+var
+  FormMain: TFormMain;
+
+implementation
+
+{$R *.fmx}
+
+procedure TFormMain.FormCreate(Sender: TObject);
+begin
+  FormMain := self;
+
+{$ifdef debug}
+  ReportMemoryLeaksOnShutdown := True;
+{$endif}
+
+  FormatSettings.DecimalSeparator := '.';
+  Caption := 'Federgraph ' + Application.Title.ToUpper;
+
+end;
+
+end.
